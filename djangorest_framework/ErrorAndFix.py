e190d74f5ccf06f6d73d48904373621b0a095182
damiemmy <damisaemmanuel778@gmail.com>
2026-04-27 21:40:33 +0100

    resolved: Error 403 forbidden error

---
+++ djangorest_framework/ErrorAndFix.py
@@ -0,0 +1,6 @@
+1.)Error:403-forbidden error:
+    Problem Description:
+        Today i created an Access Tokens and tested it with POSTMAN to  make a request to an endpoint but i got a 403 forbidden This error,it took me 1 hours of debuggin
+    Solution:
+        i configured settings.REST_FRAMEWORK correctly with right syntax
+
