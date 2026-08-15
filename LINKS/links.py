f8563f553b42448725e9e235427c3c87daf96896
damiemmy <damisaemmanuel778@gmail.com>
2026-08-15 23:19:47 +0100

    doc: link to ai videos

--- LINKS/links.py
+++ LINKS/links.py
@@ -1,2 +1,3 @@
 https://github.com/settings/tokens: github PAT configuration
 https://aistudio.google.com/prompts/new_chat: Gemini
+https://www.youtube.com/watch?v=ygXn5nV5qFc&t=18177s: youtube video
